b5ca9e7436a771e1dc6b5bacf0ecc9b5a18ccc4d
Ashley <ashpfeif12@gmail.com>
2026-04-24 16:20:44 -0400

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // works for GitHub Pages, Vercel, Netlify, static hosts
+  base: '/cratedigging/', // works for GitHub Pages, Vercel, Netlify, static hosts
 })
